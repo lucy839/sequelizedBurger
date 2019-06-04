1f37347a3d178f3dc8da460dc283d7a3f32e34fe
Youngju Lee <lucylee839@hotmail.com>
2019-06-04 11:02:33 -0700

    built sql structure in sequelize

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,38 @@
-// Import the ORM to create functions that will interact with the database.
-var orm = require("../config/orm.js");
-
-var burger = {
-    // function to select all burgers
-    selectAll: function(cb) {
-      orm.selectAll("burgers", function(res) {
-        cb(res);
-      });
+module.exports = function (sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burgers_name: {
+      type: DataTypes.STRING
     },
-
-    // function to insert one new data
-    insertOne: function(cols, vals, cb) {
-      orm.insertOne("burgers", cols, vals, function(res) {
-        cb(res);
-      });
-    },
-
-    // function to update one existing data
-    updateOne: function(burger_id, callback){
-      orm.updateOne("burgers",burger_id, function(res){
-        callback(res);
-      });
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
     }
-  };
+  });
+  return Burger;
+};
+
+// var burger = {
+//     // function to select all burgers
+//     selectAll: function(cb) {
+//       orm.selectAll("burgers", function(res) {
+//         cb(res);
+//       });
+//     },
+
+//     // function to insert one new data
+//     insertOne: function(cols, vals, cb) {
+//       orm.insertOne("burgers", cols, vals, function(res) {
+//         cb(res);
+//       });
+//     },
+
+//     // function to update one existing data
+//     updateOne: function(burger_id, callback){
+//       orm.updateOne("burgers",burger_id, function(res){
+//         callback(res);
+//       });
+//     }
+//   };
   
-  // Export the database functions for the controller (burgers_controller.js).
-  module.exports = burger;
+//   // Export the database functions for the controller (burgers_controller.js).
+//   module.exports = burger;
